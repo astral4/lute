a6ea4a787f1e25119275f32284f145e79cef706b
astral4 <88992929+astral4@users.noreply.github.com>
2026-08-17 22:51:00 -0400

    implement DoubleEndedIterator and use slice specialization

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,8 +4,13 @@ All notable changes to this project will be documented in this file.
 
 ## [Unreleased]
 
+### Added
+
+- Map and set iterators (`MapEntries`, `MapKeys`, `MapValues`, and `SetEntries`) now implement `DoubleEndedIterator`.
+
 ### Changed
 
+- `Iterator::count`, `Iterator::last`, and `Iterator::nth` for map and set iterators are now `O(1)` time instead of `O(n)` time.
 - Queries and construction now use a [PTHash](https://github.com/jermp/pthash)/[PtrHash](https://github.com/RagnarGrootKoerkamp/PTRHash)-style per-bucket pilot table instead of [CHD](https://cmph.sourceforge.net/chd.html) displacement pairs.
 - Queries and construction now use a "packed" strategy instead of a "direct" strategy. The packed strategy searches for a bit window that separates keys' hashes into distinct slots. The direct strategy searches for an RNG seed where keys hash to distinct slots, which takes more computational effort.
 
--- crates/lute_core/src/iter.rs
+++ crates/lute_core/src/iter.rs
@@ -3,102 +3,115 @@
 use core::iter::FusedIterator;
 use core::slice::Iter;
 
-/// An iterator over the key-value pairs of a [`Map`](crate::Map).
-///
-/// Created by [`Map::entries`](crate::Map::entries).
-#[derive(Clone, Debug)]
-pub struct MapEntries<'a, K, V> {
-    pub(crate) inner: Iter<'a, (K, V)>,
+/// Defines an iterator that yields `$project` applied to each element of an inner iterator.
+macro_rules! projecting_iter {
+    (
+        $(#[$doc:meta])*
+        $name:ident<$lt:lifetime $(, $param:ident)*>($inner:ty) -> $item:ty,
+        $project:expr
+    ) => {
+        $(#[$doc])*
+        #[derive(Clone, Debug)]
+        pub struct $name<$lt $(, $param)*> {
+            pub(crate) inner: $inner,
+        }
+
+        impl<$lt $(, $param)*> Iterator for $name<$lt $(, $param)*> {
+            type Item = $item;
+
+            #[inline]
+            fn next(&mut self) -> Option<Self::Item> {
+                self.inner.next().map($project)
+            }
+
+            #[inline]
+            fn size_hint(&self) -> (usize, Option<usize>) {
+                self.inner.size_hint()
+            }
+
+            #[inline]
+            fn count(self) -> usize {
+                self.inner.count()
+            }
+
+            #[inline]
+            fn last(self) -> Option<Self::Item> {
+                self.inner.last().map($project)
+            }
+
+            #[inline]
+            fn nth(&mut self, n: usize) -> Option<Self::Item> {
+                self.inner.nth(n).map($project)
+            }
+
+            #[inline]
+            fn fold<B, F>(self, init: B, f: F) -> B
+            where
+                F: FnMut(B, Self::Item) -> B,
+            {
+                self.inner.map($project).fold(init, f)
+            }
+        }
+
+        impl<$lt $(, $param)*> DoubleEndedIterator for $name<$lt $(, $param)*> {
+            #[inline]
+            fn next_back(&mut self) -> Option<Self::Item> {
+                self.inner.next_back().map($project)
+            }
+
+            #[inline]
+            fn nth_back(&mut self, n: usize) -> Option<Self::Item> {
+                self.inner.nth_back(n).map($project)
+            }
+
+            #[inline]
+            fn rfold<B, F>(self, init: B, f: F) -> B
+            where
+                F: FnMut(B, Self::Item) -> B,
+            {
+                self.inner.map($project).rfold(init, f)
+            }
+        }
+
+        impl<$lt $(, $param)*> ExactSizeIterator for $name<$lt $(, $param)*> {
+            #[inline]
+            fn len(&self) -> usize {
+                self.inner.len()
+            }
+        }
+
+        impl<$lt $(, $param)*> FusedIterator for $name<$lt $(, $param)*> {}
+    };
 }
 
-impl<'a, K, V> Iterator for MapEntries<'a, K, V> {
-    type Item = (&'a K, &'a V);
-
-    #[inline]
-    fn next(&mut self) -> Option<Self::Item> {
-        self.inner.next().map(|(k, v)| (k, v))
-    }
-
-    #[inline]
-    fn size_hint(&self) -> (usize, Option<usize>) {
-        self.inner.size_hint()
-    }
+projecting_iter! {
+    /// An iterator over the key-value pairs of a [`Map`](crate::Map).
+    ///
+    /// Created by [`Map::entries`](crate::Map::entries).
+    MapEntries<'a, K, V>(Iter<'a, (K, V)>) -> (&'a K, &'a V),
+    |(k, v)| (k, v)
 }
 
-impl<K, V> ExactSizeIterator for MapEntries<'_, K, V> {}
-impl<K, V> FusedIterator for MapEntries<'_, K, V> {}
-
-/// An iterator over the keys of a [`Map`](crate::Map).
-///
-/// Created by [`Map::keys`](crate::Map::keys).
-#[derive(Clone, Debug)]
-pub struct MapKeys<'a, K, V> {
-    pub(crate) inner: Iter<'a, (K, V)>,
+projecting_iter! {
+    /// An iterator over the keys of a [`Map`](crate::Map).
+    ///
+    /// Created by [`Map::keys`](crate::Map::keys).
+    MapKeys<'a, K, V>(Iter<'a, (K, V)>) -> &'a K,
+    |(k, _)| k
 }
 
-impl<'a, K, V> Iterator for MapKeys<'a, K, V> {
-    type Item = &'a K;
-
-    #[inline]
-    fn next(&mut self) -> Option<Self::Item> {
-        self.inner.next().map(|(k, _)| k)
-    }
-
-    #[inline]
-    fn size_hint(&self) -> (usize, Option<usize>) {
-        self.inner.size_hint()
-    }
+projecting_iter! {
+    /// An iterator over the values of a [`Map`](crate::Map).
+    ///
+    /// Created by [`Map::values`](crate::Map::values).
+    MapValues<'a, K, V>(Iter<'a, (K, V)>) -> &'a V,
+    |(_, v)| v
 }
 
-impl<K, V> ExactSizeIterator for MapKeys<'_, K, V> {}
-impl<K, V> FusedIterator for MapKeys<'_, K, V> {}
-
-/// An iterator over the values of a [`Map`](crate::Map).
-///
-/// Created by [`Map::values`](crate::Map::values).
-#[derive(Clone, Debug)]
-pub struct MapValues<'a, K, V> {
-    pub(crate) inner: Iter<'a, (K, V)>,
+projecting_iter! {
+    /// An iterator over the values of a [`Set`](crate::Set).
+    ///
+    /// Created by [`Set::entries`](crate::Set::entries).
+    SetEntries<'a, T>(MapKeys<'a, T, ()>) -> &'a T,
+    |value| value
 }
-
-impl<'a, K, V> Iterator for MapValues<'a, K, V> {
-    type Item = &'a V;
-
-    #[inline]
-    fn next(&mut self) -> Option<Self::Item> {
-        self.inner.next().map(|(_, v)| v)
-    }
-
-    #[inline]
-    fn size_hint(&self) -> (usize, Option<usize>) {
-        self.inner.size_hint()
-    }
-}
-
-impl<K, V> ExactSizeIterator for MapValues<'_, K, V> {}
-impl<K, V> FusedIterator for MapValues<'_, K, V> {}
-
-/// An iterator over the values of a [`Set`](crate::Set).
-///
-/// Created by [`Set::entries`](crate::Set::entries).
-#[derive(Clone, Debug)]
-pub struct SetEntries<'a, T> {
-    pub(crate) inner: MapKeys<'a, T, ()>,
-}
-
-impl<'a, T> Iterator for SetEntries<'a, T> {
-    type Item = &'a T;
-
-    #[inline]
-    fn next(&mut self) -> Option<Self::Item> {
-        self.inner.next()
-    }
-
-    #[inline]
-    fn size_hint(&self) -> (usize, Option<usize>) {
-        self.inner.size_hint()
-    }
-}
-
-impl<T> ExactSizeIterator for SetEntries<'_, T> {}
-impl<T> FusedIterator for SetEntries<'_, T> {}
